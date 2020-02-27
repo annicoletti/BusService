2e6935575672bbb1b0a639f4ea8fbfb4690ce238
annicoletti <andre.nicoletti@live.com>
2020-02-26 23:15:17 -0300

    BUS

--- src/main/java/br/com/nicoletti/busservice/service/impl/ClimaTempoCitiesBean.java
+++ src/main/java/br/com/nicoletti/busservice/service/impl/ClimaTempoCitiesBean.java
@@ -6,7 +6,9 @@ import java.util.Set;
 import javax.ejb.EJB;
 import javax.ejb.Stateless;
 
-import br.com.cpqd.avm.sdk.v1.builder.impl.Builder;
+import br.com.cpqd.avm.sdk.v1.builder.impl.ResponseBuilder;
+import br.com.cpqd.avm.sdk.v1.enums.EnumResponseSuccess;
+import br.com.cpqd.avm.sdk.v1.enums.EnumType;
 import br.com.cpqd.avm.sdk.v1.exception.SdkExceptions;
 import br.com.cpqd.avm.sdk.v1.model.to.Content;
 import br.com.cpqd.avm.sdk.v1.model.to.Menu;
@@ -37,45 +39,45 @@ public class ClimaTempoCitiesBean implements Integration {
 			Set<Content> contents = new HashSet<Content>();
 			int n = 1;
 			for (ClimaTempoCityTO city : cities.getCities()) {
-				Content content = builderService.CONTENT()
+				Content content = DataModelBuilder.CONTENT()
 						.addMatch(String.valueOf(city.getId()))
 						.addValue(String.valueOf(n))
-						.addText(city.getName().trim().concat("/").concat(city.getState().trim().concat(" - ").concat(city.getCountry().trim())))
+						.addText(city.getName().trim()
+								.concat("/")
+								.concat(city.getState().trim()
+								.concat(" - ")
+								.concat(city.getCountry().trim())))
 						.build();
 				contents.add(content);
 				n++;
 			}
 
 			//Constroi o objeto MENU
-			Menu menu = builderService.MENU()
+			Menu menu = DataModelBuilder.MENU()
 					.addTitle("Cidades encontrada")
 					.addContent(contents)
 					.build();
 
 			//Constroi o objeto RESPONSE
-			responseTO = Builder.RESPONSE
-					.addStatus(true)
+			responseTO = ResponseBuilder.RESPONSE_SUCCESS
 					.addRequestId(requestTO.getRequestId())
-					.addResponse(Integration.KEY_STATUS, true)
-					.addResponse(Integration.KEY_DATA_MODEL, menu)
-					.addResponse(Integration.KEY_EVENT_NAME, requestTO.getAction())
+					.addDataModel(menu)
+					.addEventName(requestTO)
+					.addType(EnumType.MENU)
+					.addResponse(EnumResponseSuccess.STATUS, true)					
 					.build();
 			return responseTO;
 		}
+		
 		if (!result.isStatus()) {
-			System.out.println("7777777777777777 SERÁ");
 			ClimatempoBadRequestTO failed = (ClimatempoBadRequestTO) result;		
-			responseTO = Builder.RESPONSE
-					.addStatus(true)
+			responseTO = ResponseBuilder.RESPONSE_ERROR
 					.addRequestId(requestTO.getRequestId())
-					.addResponse(Integration.KEY_STATUS, false)
-					.addResponse(Integration.KEY_ERROR_CODE, "XXXX")
-					.addResponse(Integration.KEY_ERROR_MESSAGE, failed.getDetail())
-					.addResponse(Integration.KEY_EVENT_NAME, requestTO.getAction())
+					.addMessage(failed.getDetail())
 					.build();					
 			return responseTO;
 		}
-		System.out.println("NENHUM RETORNO");
+		
 		return null;
 	}
 
